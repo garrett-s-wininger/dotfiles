95600c31b6542c0865317457648eac3954a8e80e
Garrett Wininger <garrett.s.wininger@outlook.com>
2023-08-29 19:20:43 -0400

    Added fuzzy-finding via telescope to NVim config

--- init.lua
+++ init.lua
@@ -9,3 +9,19 @@ vim.opt.softtabstop = 0
 vim.cmd [[
 au BufRead,BufNewFile *.java setlocal tabstop=2 shiftwidth=2
 ]]
+
+-- Plugin requests
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+
+-- Fuzzy finding
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
+Plug 'nvim-lua/plenary.nvim'
+Plug('nvim-telescope/telescope.nvim', { tag = '0.1.2'})
+
+vim.call('plug#end')
+-- End plugin requests
+
+-- Telescope configuration
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
