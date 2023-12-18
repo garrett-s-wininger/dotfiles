d19d83734813f4a8787e3d7767f828930e850f65
Garrett Wininger <garrett.s.wininger@gmail.com>
2023-12-17 21:41:36 -0500

    Add John the Ripper, Evil Win-RM, and Responder to HTB Nix derivation

--- nix/htb.nix
+++ nix/htb.nix
@@ -1,12 +1,15 @@
 with (import <nixpkgs> {});
 mkShell {
     buildInputs = [
+        evil-winrm
         firefox
         gobuster
         inetutils
+        john
         mysql
         nmap
         redis
+        responder
         samba
     ];
 }
