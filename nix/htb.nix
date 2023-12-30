d0c676143029bd08d9c6028d3b3774c0281e97a9
Garrett Wininger <garrett.s.wininger@gmail.com>
2023-12-30 14:58:03 -0500

    Additional pentest tooling

--- nix/htb.nix
+++ nix/htb.nix
@@ -3,8 +3,11 @@ mkShell {
     buildInputs = [
         awscli2
         evil-winrm
+        feroxbuster
+        ffuf
         firefox
         gobuster
+        hashcat
         inetutils
         john
         metasploit
@@ -16,5 +19,9 @@ mkShell {
         responder
         samba
         sqlite
+        sqlmap
+        tshark
+        wfuzz
+        wireshark
     ];
 }
