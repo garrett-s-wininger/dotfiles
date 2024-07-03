738acc8bd86cec5ffb9a5f31bd70ac9d05fe3819
Garrett Wininger <garrett.s.wininger@outlook.com>
2024-07-03 01:03:10 -0400

    Add a basic Emacs configuration

---
+++ emacs/README.md
@@ -0,0 +1,7 @@
+# Emacs
+
+This directory contains my primary Emacs configuration for developing in a
+number of languages. To apply this to your local machine, you can simply
+link it via `ln -s "${CHECKOUT_DIR}/emacs/init.el" ~/.emacs.d/init.el`.
+At that point, you can either load the file via `M-X load-file` or close
+and reopen Emacs for the changes to take effect.
---
+++ emacs/init.el
@@ -0,0 +1,58 @@
+;; Manual settings avoid custom-set-variable overwrite
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+;; Base package and archive configuration
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa", "https://stable.melpa.org/packages/")
+	     t)
+
+(setq package-selected-packages
+      '(company ;
+	consult ;
+	magit ;
+	marginalia ;
+	vertico))
+
+(package-install-selected-packages t)
+(package-autoremove)
+
+(require 'use-package)
+
+;; Git integration
+(use-package magit
+  :commands magit)
+
+;; Minibuffer improvements
+(use-package consult
+  :hook (completion-list-mode . consult-preview-at-point-mode))
+
+(use-package vertico
+  :init (vertico-mode))
+
+(use-package marginalia
+  :bind (:map minibuffer-local-map
+	      ("M-A" . marginalia-cycle))
+  :init (marginalia-mode))
+
+;; Completion
+(use-package company
+  :hook prog-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(display-line-numbers 'relative)
+ '(display-line-numbers-type 'relative)
+ '(global-display-line-numbers-mode nil)
+ '(menu-bar-mode nil)
+ '(package-selected-packages '(lua-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(line-number-current-line ((t (:inherit line-number :foreground "white" :underline t :weight bold)))))
