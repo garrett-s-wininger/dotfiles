d4de1031ee62671773550a1c7aa2a98f5e66546f
Garrett Wininger <garrett.s.wininger@outlook.com>
2024-07-03 23:19:01 -0400

    Move list to proper table for Oil dependency

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -42,7 +42,7 @@ return {
         "stevearc/oil.nvim",
         cmd = "Oil",
         dependencies = {
-            "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font
+            { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font }
         },
         config = {}
     },
