26783098537d25529e5f5f6d07921b98ec4684cc
Garrett Wininger <garrett.s.wininger@outlook.com>
2025-01-18 17:50:41 -0500

    Remove unused plugins, LSP from NVim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,11 +1,9 @@
 return {
     require("specs.comment"),
-    require("specs.todo_comments"),
     require("specs.lualine"),
     require("specs.oil"),
     require("specs.telescope"),
     require("specs.neogit"),
-    require("specs.lsp_config"),
     require("specs.treesitter"),
     require("specs.dracula"),
 }
--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -1,11 +1,6 @@
 -- Kebinding - Easily Remove Search Highlight
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Keybinding - Diagnostics
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
-
 -- Keybinding - Navigate Different Windows/Panes
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
